a76502f0c2da94559fa1b72b5e582d8d73398de6
dhruvagrawal1055 <75847784+dhruvagrawal1055@users.noreply.github.com>
2023-02-24 21:36:55 +0530

    some changes in image generation

---
+++ your_own_ai/lib/openAi/ImageGeneration/image_response.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+
+class ImageResponse {
+  final int created;
+  final List<dynamic>? data;
+  const ImageResponse({
+    required this.created,
+    required this.data,
+  });
+
+factory ImageResponse.fromResponse(Response response ) {
+     Map<String, dynamic> responseBody = json.decode(response.body);
+     List<dynamic> data = responseBody['data'];
+  return ImageResponse(
+    created: json['created'] as int,
+    data: json['data'] as List<dynamic>?,
+
+  );
+}
+}
